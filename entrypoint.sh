e8bac89d97a2eedf75128a5ab194bad56cacdcb6
BlueEnni <enricobachus@t-online.de>
2020-08-17 09:31:34 +0200

    added timezone

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#copy files to mounted folder /data
 shopt -s extglob
 mv /files/!(entrypoint.sh) /data
 rm -R /files/!(entrypoint.sh)
+#add timezone
+apk add tzdata
+cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+apk del tzdata
 # Start the java process
 /opt/java/openjdk/bin/java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS /data/${JARFILE} --nojline nogui
 status=$?
